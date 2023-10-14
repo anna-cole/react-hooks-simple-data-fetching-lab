137de681ba47c3d13f543d9e1584d1faf22bde1e
Anna Raptidou Cole <acrrj123@gmail.com>
2023-10-13 21:23:26 -0500

    Lab done final

--- src/components/App.js
+++ src/components/App.js
@@ -8,15 +8,12 @@ function App() {
     fetch('https://dog.ceo/api/breeds/image/random')
     .then(r => r.json())
     .then(data => setImage(data.message))
-  })
+  }, [])
+  // use an empty dependencies array, so we only run the fetch request ONCE
 
   if(!image) return <p>Loading...</p>
 
-  return (
-    <div>
-      <img src={image} alt='A Random Dog'/>
-    </div>
-  )
+  return <img src={image} alt='A Random Dog'/>
 }
 
 export default App
